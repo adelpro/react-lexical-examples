99181bf27a704a4531a98085fa6eb7480beb456d
adelpro <47066151+adelpro@users.noreply.github.com>
2023-12-09 20:39:14 +0100

    Update CustomHeadingActions.tsx
    Make the heading button action reversible , clicking on h1 for example on a heading node will convert it to paragraph node

--- src/components/CustomHeadingActions/CustomHeadingActions.tsx
+++ src/components/CustomHeadingActions/CustomHeadingActions.tsx
@@ -3,12 +3,25 @@ import { HeadingTagType } from '@lexical/rich-text';
 import { FORMAT_HEADING_COMMAND } from '../../plugins';
 import { ActionsContainer } from '../ActionsContainer';
 import { ActionButton } from '../ActionButton';
+import { $createParagraphNode, $getSelection, $isRangeSelection } from 'lexical';
+import { $createHeadingNode, $isHeadingNode } from '@lexical/rich-text';
+import { $setBlocksType } from '@lexical/selection';
 
 export const CustomHeadingActions = () => {
    const [editor] = useLexicalComposerContext();
-
+   // Make it reversible
    const handleOnClick = (tag: HeadingTagType) => {
-      editor.dispatchCommand(FORMAT_HEADING_COMMAND, tag);
+      editor.update(() => {
+      const selection = $getSelection();
+      if (!$isRangeSelection(selection)) return;
+      const parentNode = selection.anchor.getNode().getParent();
+      const isHeading = $isHeadingNode(parentNode);
+      if (isHeading) {
+        $setBlocksType(selection, () => $createParagraphNode());
+      } else {
+        $setBlocksType(selection, () => $createHeadingNode(tag));
+      }
+    });    
    };
 
    return (
